343637a4c5f32c2bbc052050eb86e52a17693807
Ian Forbes <ian.forbes@mail.mcgill.ca>
2012-06-15 00:59:48 -0300

    Fixed syntax error.

--- src/semaphore.h
+++ src/semaphore.h
@@ -1,5 +1,13 @@
+#include "eThread.h"
+
+typedef struct _waitQueue{
+	struct _waitQueue* next;
+	eThread* queued;
+}waitQueue;
+
 typedef struct _semaphore{
 	int value;
+	waitQueue* blocked;
 }semaphore;
 
 int semaphore_create(semaphore*, int);
