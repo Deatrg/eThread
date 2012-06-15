490c044f87ad1a9c0f1bc5f37f9e264b187326ef
Ian Forbes <ian.forbes@mail.mcgill.ca>
2012-06-15 00:58:14 -0300

    Rough version of eThread_create complete.

--- src/eThread.c
+++ src/eThread.c
@@ -1,9 +1,25 @@
 #include "eThread.h"
 
-void eThread_create(void(*function)(void) , int stackSize){
-	return;
+int eThread_create(eThread* target, void(*function)(void) , int stackSize){
+	static int numThreads = 0;
+	if(runningThread == NULL){
+		runningThread = target;
+	}
+	else{
+		runningThread->next = target;
+	}
+	getcontext(&(target->context));
+	target->threadID = numThreads++;
+	target->state = RUNNABLE;
+	target->next = NULL;
+	target->context.uc_stack.ss_sp = calloc(stackSize, sizeof(char));
+	target->context.uc_stack.ss_size = stackSize;
+	target->context.uc_link = &(mainThread->context);
+	sigemptyset(&(target->context.uc_sigmask));
+	makecontext(&(target->context), function, 0, NULL);
+	return 0;
 }
 
-void eThread_destory(void){
-	return;
+int eThread_destory(void){
+	return 0;
 }
