9e021af38502910f147f0a9ff8f696609c8e38e6
Ian Forbes <ian.forbes@mail.mcgill.ca>
2012-06-25 19:51:37 -0300

    Introduced idle thread to handle scheduling.

--- src/eThread.h
+++ src/eThread.h
@@ -1,33 +1,36 @@
-#include <time.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <ucontext.h>
-
 #ifndef ETHREAD
 #define ETHREAD
+
+#include <stdio.h>
+#include <signal.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ucontext.h>
+#include <sys/time.h>
+
+#define	RUNNING		0
+#define	RUNNABLE	1
+#define	BLOCKED		2
+#define	EXIT		3
+
 typedef struct _eThread{
 	ucontext_t context;
 	int threadID;
 	int state;
 	struct _eThread* next;
 }eThread;
-#endif
-
-enum STATES{
-	RUNNING,
-	RUNNABLE,
-	BLOCKED,
-	EXIT
-};
 
 static eThread* runQueue;	//Points to head of runQueue
 static eThread* runningThread;	//Points to currently running thread in the runQueue
-static eThread* mainThread;	//Points back to the main thread of execution
+static eThread  mainThread;	//Contains the context of the main thread of execution
+static eThread	idleThread;	//Does All Schduling
 
-static int timeQuantum;
+static int 	timeQuantum;
 
-int eThread_create(eThread*, void(*)(void), int);
-int eThread_destroy(void);
-int eThread_yield(void);
-int eThread_setQuantum(int);
-static void scheduler(int);
+int 	eThread_create(eThread*, void(*)(void), int);
+void 	eThread_exit(void);
+int 	eThread_yield(void);
+int 	eThread_setQuantum(int);
+void 	eThread_init(void);
+void	scheduler(int);
+#endif
